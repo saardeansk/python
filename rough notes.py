6021f779a1a9c9a17f0e48408ac85575a370add8
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-09-16 18:27:34 +0100

    rough notes.py
    summary of full course

---
+++ rough notes.py
@@ -0,0 +1,13 @@
+                            PYTHON COURSE
+ 1. basics
+What is Python?
+   Interpreted, high-level, portable, dynamically typed.
+Python Syntax
+   Indentation (no {} like C/C++/Java).
+Comments:
+   # single-line, """ multi-line """.
+Variables
+   No need to declare type, Python assigns automatically.
+Input/Output=input()/print()
+   Always get the input from user in string no need any changes in syntax input("enter the name")
+   otherwise get in or anything rather string casting use  input(int("enter the number"))
