f36ec2f40ed48c132eb5b32d2e1d9ff61bcbc473
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-09-16 19:27:48 +0100

    swap.py
    swap using the xor and sum logic

--- swap.py
+++ swap.py
@@ -1,4 +1,9 @@
-                   swaping logic
+                   swaping logic  using  XOR  (or)  sum logic
+             ( XOR )
+   a=a^b
+   b=a^b
+   a=a^b
+example
 a=5,b=10
  0101
  1010
@@ -12,6 +17,12 @@ b=5
   0101
   1010
 a=10
+  (SUM LOGIC)
+ SUM=a+b
+a=sum-a
+b=sum-b
+  swapped
+                                   example program
    n=int(input("enter the number of times to swap the variable : "))
 for i in range(n):
     a=int(input("enter the value of a : "))
@@ -27,6 +38,5 @@ for i in range(n):
         print("the value of a and b: ",a,b)
         a=sm-a
         b=sm-b
-        
         print("the swapped value is ",a,b)
        
