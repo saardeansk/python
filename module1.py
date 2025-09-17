f75abd33315b337a125a6a552721c3899c39792d
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-09-17 19:39:38 +0100

    module1.py
    basics like operators,loops etc..

---
+++ module1.py
@@ -0,0 +1,10 @@
+number=int(input("enter the number of custmer"))
+for i in range(number):
+  bill=int(input("bill amount"))
+  if(bill >= 500):
+   print(bool(1))
+   print(f"discount applied, 30%")
+  else:
+   print(bool(0))
+   print("no discount,  you need discount purchase things above 500")
+    
