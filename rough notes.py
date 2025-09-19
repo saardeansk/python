a2aed2e05bcd5e7e396d23271009dcf443bb458d
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-09-19 18:47:11 +0100

    rough notes.py

--- rough notes.py
+++ rough notes.py
@@ -7,7 +7,7 @@ Python Syntax
 Comments:
    # single-line, """ multi-line """.
 Variables
-   No need to declare type, Python assigns automatically.
+   No need to declare type, Python assigns automatically  .....
 Input/Output=input()/print()
    Always get the input from user in string no need any changes in syntax input("enter the name")
    otherwise get in or anything rather string casting use  input(int("enter the number"))
