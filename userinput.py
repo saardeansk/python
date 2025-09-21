1645411f41bd8c9fcc0b2e2d79c62b62f96aae8e
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-09-21 17:58:14 +0100

    userinput.py

--- userinput.py
+++ userinput.py
@@ -1,5 +1,6 @@
-a=input("enter the name:")
-b=input("enter the age of the student:")
-print("hello",a,"Happy" ,b,"th Birthday ")
 c=input(int("Enter the number of times to use the program"))
-      
+for i in range(i,c):
+ a=input("enter the name:")
+ b=input("enter the age of the student:")
+ print("hello",a,"Happy" ,b,"th Birthday ")
+
