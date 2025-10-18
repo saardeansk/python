790e4b2f0cd38c30b72eb10d3c9f58bb915d8086
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-10-18 16:11:11 +0530

    Update rough notes.py

--- rough notes.py
+++ rough notes.py
@@ -1,7 +1,7 @@
                             PYTHON COURSE
  1. basics
 What is Python?
-   Interpreted , high-level  , portable, dynamically  typed.
+   Interpreted , high-level  , port able, dynamically  typed.
 syntax
    Indentation (no {} like  C/C++/Java).
 Comments:
