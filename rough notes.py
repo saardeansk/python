6cff92a71eb6e35f6a14309f21010b269bbf1dbb
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-10-19 19:35:13 +0530

    Update rough notes.py

--- rough notes.py
+++ rough notes.py
@@ -10,4 +10,4 @@ Variables
    No need  to declare  type, Python  assigns automatically  .....
 Input/Output=input()/print()
    Always get  the  input from  user   in  string no need any changes in syntax input("enter the name")
-   oth. erwise  get in (or)anything   rather string casting  use   input(int("enter the number"))
+   otherwise  get in (or)anything   rather string casting  use   input(int("enter the number"))
