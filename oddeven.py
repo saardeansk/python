6237c5eeb5b56f0494dd1a83163c82289d3fc3c2
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-11-30 03:26:57 +0000

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,4 +1,4 @@
-n=int(input("Enter   the   Number of times run program:"))
+n=int(input("Enter   the   Number  of times run program:"))
 for i in range(n):
  a=int(input("enter  the number:"))
  if a%2==0:
