1cfaa3e3c31e2e9277afc37e1d058afc09b40f8b
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2025-11-30 03:26:22 +0000

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -2,7 +2,7 @@ n=int(input("Enter   the   Number of times run program:"))
 for i in range(n):
  a=int(input("enter  the number:"))
  if a%2==0:
-      print("the  given ",a,"is even")
+      print("the  given  ",a,"is even")
  elif a%2!=0:
       print("the given ",a,"  is odd")
   else 
