4a7f49ad248d44b0ebd857c0f0ee5de41eb1fd85
SATHISHKUMAR.S <sathishkumarsk2210@gmail.com>
2026-09-07 17:39:27 +0530

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -1,7 +1,10 @@
  1. INTEGER INPUT WITH THE SINGLE VALUE
-c = input(int("Enter  the number of times to use the program"))
-for i in range(i,c):
- a=input("enter  the  name:")
- b=input("enter the age  of the student  :")
- print("hello",a,"Happy" ,b,"th Birthday ")
+  c = input(int("Enter  the number of times to use the program"))
+  for i in range(i,c):
+      a=input("enter  the  name:")
+      b=input("enter the age  of the student  :")
+      print("hello",a,"Happy" ,b,"th Birthday ")
 
+2. LIST OF VALUES STORED WHICH  CAN CONVERT INTO LIST VALUES AND SEPARATE EACH VALUE USING THE SPLIT() FUNCTION
+    c= list (map(int,input("enter the value").split()))
+   
